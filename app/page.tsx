a9acd4f7b26c8795161c6265e3301b7c92fcecb7
naveedbasharat06 <naveed.basharat06@gmail.com>
2025-11-29 00:11:08 +0500

    updated code

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <h2>Creating shareable on social media url with next</h2>
-     share property on social preivew 
+     share property on social preivew ..
     </div>
   );
 }
