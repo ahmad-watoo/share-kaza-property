62924a99c270483f1cd95397ac77e57b70f5cc6f
naveedbasharat06 <naveed.basharat06@gmail.com>
2025-11-28 21:55:41 +0500

    Initial commit from Create Next App

---
+++ next.config.ts
@@ -0,0 +1,13 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  images: {
+    domains: [
+      "uuypavxadszssptaicvt.supabase.co", // replace with your storage domain(s)
+      "kazaswap-og.onrender.com", // if used
+    ],
+  },
+};
+// https://uuypavxadszssptaicvt.supabase.co
+export default nextConfig;
